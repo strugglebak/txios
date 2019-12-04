deaf7f37b50629904205c8375e027c7553b16877
strugglebak <strugglebak@gmail.com>
2019-12-04 11:35:54 +0800

    新增 jest 配置

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "verbose": true,
+  "transform": {
+    ".(ts|tsx)": "ts-jest"
+  },
+  "testEnvironment": "jsdom",
+  "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  "coveragePathIgnorePatterns": [
+    "/node_modules/",
+    "/test/"
+  ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 90,
+      "functions": 95,
+      "lines": 95,
+      "statements": 95
+    }
+  },
+  "collectCoverageFrom": [
+    "src/*.{js,ts}",
+    "src/**/*.{js,ts}"
+  ],
+  "setupFilesAfterEnv": [
+    "<rootDir>/test/boot.ts"
+  ]
+}
